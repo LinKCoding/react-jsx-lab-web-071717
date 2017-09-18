37820b794a009507ee57c1495a32f9de3b2925af
linkcoding <kenny.lin.91@gmail.com>
2017-09-18 10:42:44 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,17 @@
+import FillerText from './FillerText';
+import React from 'react';
+
+
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <title>"The world's coolest webpage"</title>
+        <FillerText />
+        <FillerText />
+      </div>
+    )
+  }
+}
+
+export default Webpage;
